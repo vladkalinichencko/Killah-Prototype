caebeb53addc6e0c1c8609a77486093a48a17e90
MMenshikh <150797125+MMenshikh@users.noreply.github.com>
2025-06-19 17:33:11 +0300

    Update utils.py

--- Jupyter Notebooks/utils.py
+++ Jupyter Notebooks/utils.py
@@ -1,14 +1,19 @@
-from pydub import AudioSegment
+import subprocess
 from pathlib import Path
 
 def convert_to_wav(input_path: Path, output_path: Path, sample_rate=16000):
     try:
-        audio = AudioSegment.from_file(input_path)
-        audio = audio.set_frame_rate(sample_rate).set_channels(1)
         output_path.parent.mkdir(parents=True, exist_ok=True)
-        audio.export(output_path, format="wav")
-        return True
+        command = [
+            "ffmpeg",
+            "-y",  # перезаписывать без запроса
+            "-i", str(input_path),
+            "-ar", str(sample_rate),
+            "-ac", "1",  # моно
+            str(output_path)
+        ]
+        result = subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return result.returncode == 0
     except Exception as e:
-        print(f"Conversion error: {input_path} -> {e}")
+        print(f"FFmpeg error on {input_path}: {e}")
         return False
-      
